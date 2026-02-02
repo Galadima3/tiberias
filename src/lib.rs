c5e61bb0b5f3c2b03b60c005caa5ca5c700b09a2
Galadima3 <abrahamgaladima300@gmail.com>
2026-02-02 06:45:39 +0100

    2nd Commit

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod stats;
 pub mod pig_latin;
 
 pub use stats::*;
-pub use pig_latin::*;
+pub use pig_latin::convert_sentence;
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,32 @@ use std::io::{self};
 use tiberias::{calculate_median, calculate_mode, convert_sentence};
 
 fn main() {
-    let numbers = [1,2,3,4,5,6,7,8,9,10];
-    let numbers1 = [1,3,3,4,5,5,7,8,9,5];
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let numbers1 = [1, 3, 3, 4, 5, 5, 7, 8, 9, 5];
 
-    let median = calculate_median(&numbers);
-    match median {
-        Some(median) => println!("median: {}", median),
-        None => println!("Median not obtainable"),
+    if let Some(rex) = numbers.get(5) {
+        println!("value: {rex}")
+    } else {
+        println!("couldnt get value")
     }
+
+    //let median = calculate_median(&numbers);
+    if let Some(median) = calculate_median(&numbers) {
+        println!("median: {}", median);
+    }
+    // match median {
+    //     Some(median) => println!("median: {}", median),
+    //     None => println!("Median not obtainable"),
+    // }
     let mode = calculate_mode(&numbers1);
     match mode {
         Some(mode) => println!("mode: {}", mode),
         None => println!("Mode not obtainable"),
     }
 
-
     println!("=== Pig Latin Converter ===\n");
     loop {
         println!("Enter a word or sentence (or 'quit' to exit): ");
-
 
         let mut input = String::new();
         io::stdin()
